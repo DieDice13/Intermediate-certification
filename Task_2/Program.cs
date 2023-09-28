06c96b92d88266e42723d5b1862b03463a38ddf4
Ruslan <die.dice.2003@mail.ru>
2023-09-29 03:04:01 +0600

    Task 2 is solved

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.Clear();
+
+System.Console.WriteLine("Введите значение M : ");
+int m = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение N : ");
+int n = int.Parse(Console.ReadLine());
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов = {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
