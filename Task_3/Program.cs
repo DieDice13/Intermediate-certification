4d21e97082f17b4a779a36276554b3d50d04c023
Ruslan <die.dice.2003@mail.ru>
2023-09-29 03:04:57 +0600

    Task 3 is solved

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Clear();
+
+
+System.Console.WriteLine("Введите значение M : ");
+int m = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите значение N : ");
+int n = int.Parse(Console.ReadLine());
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
