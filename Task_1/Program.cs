bc54eb3f7291c9cecfca525ba45e6d4430b227a5
Ruslan <die.dice.2003@mail.ru>
2023-09-29 02:53:40 +0600

    Task 1 is solved

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int N = 15;
+
+System.Console.WriteLine($"Значение N -> {N}");
+
+void Interval(int N, int i = 1)
+{  
+    if (i > N)
+    {
+        return;
+    }
+    Interval(N, i + 1);
+    System.Console.Write(i + ", ");
+}
+Interval(N);
